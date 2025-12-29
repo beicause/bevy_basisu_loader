8f3c2624fa71a2fe177c8472c9b78e929682ff83
Luo Zhihao <luo_zhihao@outlook.com>
2025-12-29 22:31:22 +0800

    Simplify bindings

--- crates/basisu-sys/basisu.mjs
+++ crates/basisu-sys/basisu.mjs
@@ -4,15 +4,15 @@ import Vendor from "basisu-vendor";
 const vendor = await Vendor();
 
 export function js_basisu_transcoder_init() {
-	vendor._rust_basisu_transcoder_init();
+	vendor._c_basisu_transcoder_init();
 }
 
 export function js_ktx2_transcoder_new() {
-	return vendor._rust_ktx2_transcoder_new();
+	return vendor._c_ktx2_transcoder_new();
 }
 
 export function js_ktx2_transcoder_delete(transcoder) {
-	vendor._rust_ktx2_transcoder_delete(transcoder);
+	vendor._c_ktx2_transcoder_delete(transcoder);
 }
 
 /**
@@ -31,7 +31,7 @@ export function js_ktx2_transcoder_transcode_image(
 	const src_buf = vendor._malloc(len);
 	vendor.HEAPU8.set(u8_arr, src_buf);
 
-	let success = vendor._rust_ktx2_transcoder_transcode_image(
+	let success = vendor._c_ktx2_transcoder_transcode_image(
 		transcoder,
 		src_buf,
 		len,
@@ -47,32 +47,32 @@ export function js_ktx2_transcoder_transcode_image(
  * @returns {Uint8Array}
  */
 export function js_ktx2_transcoder_get_r_dst_buf(transcoder) {
-	const dst_buf = vendor._rust_ktx2_transcoder_get_r_dst_buf(transcoder);
+	const dst_buf = vendor._c_ktx2_transcoder_get_r_dst_buf(transcoder);
 	/** @type {number} */
-	const dst_len = vendor._rust_ktx2_transcoder_get_r_dst_buf_len(transcoder);
+	const dst_len = vendor._c_ktx2_transcoder_get_r_dst_buf_len(transcoder);
 	const res_arr = new Uint8Array(dst_len);
 	res_arr.set(vendor.HEAPU8.subarray(dst_buf, dst_buf + dst_len));
 	return res_arr;
 }
 
 export function js_ktx2_transcoder_get_r_width(transcoder) {
-	return vendor._rust_ktx2_transcoder_get_r_width(transcoder);
+	return vendor._c_ktx2_transcoder_get_r_width(transcoder);
 }
 export function js_ktx2_transcoder_get_r_height(transcoder) {
-	return vendor._rust_ktx2_transcoder_get_r_height(transcoder);
+	return vendor._c_ktx2_transcoder_get_r_height(transcoder);
 }
 export function js_ktx2_transcoder_get_r_levels(transcoder) {
-	return vendor._rust_ktx2_transcoder_get_r_levels(transcoder);
+	return vendor._c_ktx2_transcoder_get_r_levels(transcoder);
 }
 export function js_ktx2_transcoder_get_r_layers(transcoder) {
-	return vendor._rust_ktx2_transcoder_get_r_layers(transcoder);
+	return vendor._c_ktx2_transcoder_get_r_layers(transcoder);
 }
 export function js_ktx2_transcoder_get_r_faces(transcoder) {
-	return vendor._rust_ktx2_transcoder_get_r_faces(transcoder);
+	return vendor._c_ktx2_transcoder_get_r_faces(transcoder);
 }
 export function js_ktx2_transcoder_get_r_target_format(transcoder) {
-	return vendor._rust_ktx2_transcoder_get_r_target_format(transcoder);
+	return vendor._c_ktx2_transcoder_get_r_target_format(transcoder);
 }
 export function js_ktx2_transcoder_get_r_is_srgb(transcoder) {
-	return vendor._rust_ktx2_transcoder_get_r_is_srgb(transcoder);
+	return vendor._c_ktx2_transcoder_get_r_is_srgb(transcoder);
 }
--- prebuilt/basisu-vendor.js
+++ prebuilt/basisu-vendor.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05570b4a5dcf59d102e7dd9995993c9461d62282badf044a8ceb4a916af8fd5d
-size 90308
+oid sha256:d0d9daceb1853fe458170bf5cb05231ed4dc8f9bedabb31babb630d171b6fd09
+size 89996
