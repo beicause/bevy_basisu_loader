7cefcf5f3a888d32e3659d7a8c01752d0abb6790
Luo Zhihao <luo_zhihao@outlook.com>
2026-01-01 11:50:14 +0800

    update

--- vendor/README.md
+++ vendor/README.md
@@ -1,1 +1,1 @@
-The `transcoder` and `zstd` directories are part of the `basis_universal` repository, managed by the `update-basis-universal.sh` script.
+The `basis_universal` directory is part of the [basis_universal](https://github.com/BinomialLLC/basis_universal) repository, managed by the `update-basis-universal.sh` script.
--- vendor/update-basis-universal.sh
+++ vendor/update-basis-universal.sh
@@ -3,7 +3,7 @@
 rm -r ./basis_universal
 mkdir ./basis_universal
 
-git clone --depth 1 https://github.com/BinomialLLC/basis_universal.git --branch v1_60_snapshot basis_universal_repo
+git clone --depth 1 https://github.com/BinomialLLC/basis_universal --branch v1_60_snapshot basis_universal_repo
 cp -r ./basis_universal_repo/transcoder/ ./basis_universal/
 cp -r ./basis_universal_repo/zstd/ ./basis_universal/
 cp ./basis_universal_repo/LICENSE ./basis_universal/
